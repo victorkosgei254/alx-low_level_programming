5300133508e554437d23c9c1ea9cfe86821f77bf
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-16 10:40:55 -0800

    Assignments

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point 
+ *
+ * Return 0 : Always
+ */
+
+int main(void)
+{
+
+
+    printf("Size of a char: %lu byte(s)\n",sizeof(char));
+    printf("Size of a int: %lu byte(s)\n",sizeof(int));
+    printf("Size of a char %lu byte(s)\n",sizeof(long));
+    printf("Size of a long int %lu byte(s)\n",sizeof(long long));
+    printf("Size of a floar : %lu bytes\n",sizeof(float));
+    return (0);
+}
