67c8e90783dc0aa58b447613526817e04aa95e85
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-16 11:15:05 -0800

    Betty style, formatting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point 
  *
@@ -7,9 +6,7 @@
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    
-    return (0);
-
+printf("with proper grammar, but the outcome is a piece of art,\n");    
+return (0);
 }
 
