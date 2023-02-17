754f6bbb63aed9acccda63c09a249bfb930c99f0
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-17 13:53:45 -0800

    Fixed file 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - Entry point 
+ * main -Entry point
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
     int i;
-    for(i = 48;i<58;i++)
+    for (i = 48; i < 58; i++)
         putchar(i);
-    for(i i=97;i<103;i++)
+    for(i = 97; i < 103; i++)
         putchar(i);
     putchar('\n');
     return (0);
