86bd90b587e12d026e3c841160e4f8ca35379bdb
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-17 13:50:14 -0800

    Betty standard file 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * main Entry point 
+ * main -Entry point 
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
     int i;
-    for(i = 48;i<58;i++)
+    for (i = 48; i < 58 ; i++)
     {
         putchar(i);
         if(i != 57)
         {
-        putchar(44);
-        putchar(32);
+            putchar(44);
+            putchar(32);
         }
     }
     putchar('\n');
