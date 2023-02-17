c2db283ba9622ed71c633bc51bd2c9903f84e4ac
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-17 14:53:10 -0800

    Fixed betty documentation

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 /**
- * main Entry point 
+ * main -Entry point
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
     int i;
-    for(i=97;i<123;i++)
-        putchar(i);
+    for (i = 97; i < 123; i++)
+    {
+         putchar(i);
+    }
     putchar('\n');
     return (0);
 }
