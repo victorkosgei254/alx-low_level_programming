e1d45aa551e37ba8af079951d8dcd42334b029f3
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-17 14:07:55 -0800

    Bug fix file3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,7 +7,9 @@
 int main(void)
 {
     int i;
-    for(i = 65;i<123;i++)
+    for(i = 65;i<91;i++)
+        putchar(i);
+    for (i = 97; i < 123; i++)
         putchar(i);
     putchar('\n');
     return (0);
