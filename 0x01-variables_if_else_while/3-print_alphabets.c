9dd0cd634fe09f83f68ed4356a934a0be627f0ca
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-17 14:10:26 -0800

    Right order

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main Entry point
+ * main -Entry point
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
     int i;
-    for(i = 65;i<91;i++)
+    for(i = 97; i < 123; i++)
         putchar(i);
-    for (i = 97; i < 123; i++)
+    for (i = 65; i < 91; i++)
         putchar(i);
     putchar('\n');
     return (0);
