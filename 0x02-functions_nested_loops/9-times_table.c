a6ad9ac9ce3d4e3ce3a88cf73c67f070b94b02ad
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-21 11:22:03 -0800

    File 9 test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -17,9 +17,9 @@ void times_table(void)
             }
             else
             {
-                _putchar(' ');
                 _putchar((i*j) + '0');
                 _putchar(',');
+                _putchar(' ');
                 _putchar(' ');
             }
         }
