956c072e2f70ff5c318534282975b784bc0b2639
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-21 11:09:14 -0800

    Fixed file 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
     {
         for( j = 0; j < 10; j++)
         {
-            _putchar(i * j);
+            _putchar((i * j) + '0');
             _putchar(',');
             _putchar(' ');
         }
