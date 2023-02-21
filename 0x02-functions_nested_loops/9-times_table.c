db2e8c92d77ab5396adebc827c82b91d78a9cc5c
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-21 11:49:02 -0800

    File 9 test

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,9 @@ void times_table(void)
 
     for( i = 0; i < 10; i++ )
     {
-        for( j = 0; j < 9; j++)
+        _putchar(0+'0');
+        _putchar(',');
+        for( j = 1; j < 9; j++)
         {
             prod = i*j;
             if(prod > 9)
@@ -17,10 +19,9 @@ void times_table(void)
             }
             else
             {
-
+                _putchar(' ');
                 _putchar((i*j) + '0');
                 _putchar(',');
-                _putchar(' ');
                 _putchar(' ');
             }
         }
