84babfcfeabf1df4a7b6de001638e9f97be1cdea
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-21 12:09:14 -0800

    File 7 test

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,15 +3,16 @@ int print_last_digit(int n)
 {
     int last_digit;
     
-    if(n < 0 )
-    {
-        n = n*-1;
-    }
-    else if(n == -2147483648)
+   if(n == -2147483648)
     {
         _putchar(8);
         return 8;
     }
+   else if(n < 0 )
+    {
+        n = n*-1;
+    }
+ 
     last_digit = n%10;
     _putchar(last_digit + '0');
     return last_digit;
