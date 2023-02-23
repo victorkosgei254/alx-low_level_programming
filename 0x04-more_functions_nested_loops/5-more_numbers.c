514d9188e713135a2a83f5e74f752b9d61c470bf
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-23 13:00:25 -0800

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
+/**
+ * more_numbers - Print more numbers
+ */
 void more_numbers(void)
 {
-    int i, j;
+	int i, j;
 
-    for( i = 0; i < 14; i++)
-    {
-        for( j = 48; j < 59; j++ )
-        {
-            _putchar(j);
-        }
-
-        for( j = 48; j < 53; j++)
-        {
-            _putchar(49);
-            _putchar(j);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 14; i++)
+	{
+		for (j = 48; j < 59; j++)
+		{
+			_putchar(j);
+		}
+		for (j = 48; j < 53; j++)
+		{
+			_putchar(49);
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
 }
