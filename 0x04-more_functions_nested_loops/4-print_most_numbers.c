b8327a0d44835b638073ec68e3067f2031f5aaa7
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-23 12:55:31 -0800

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,18 @@
 #include "main.h"
 
+/**
+ * print_most_numbers - Print most numbers
+ */
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for(i = 48; i < 59; i++)
-    {
-        if( !((i == 50) || (i == 52)) )
-        {
-            _putchar(i);
-        }
-    }
-    _putchar('\n');
-    return;
+	for (i = 48; i < 58; i++)
+	{
+		if (!((i == 50) || (i == 52)))
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
 }
