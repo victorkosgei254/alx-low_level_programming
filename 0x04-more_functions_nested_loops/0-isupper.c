5a9492bfa7ed751967e54218a65a057802ebdec7
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-23 12:32:31 -0800

    Betty sauce

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
+/**
+ * _isupper - Function to check if its upper
+ * @c: input
+ *
+ * Return: 0 if not upper, 1 if upper
+ */
 int _isupper(int c)
 {
-    if (!((c > 64) && (c < 91)))
-    {
-    return (0);
-    }
-    return (1);
+	if (!((c > 64) && (c < 91)))
+	{
+		return (0);
+	}
+	return (1);
 }
