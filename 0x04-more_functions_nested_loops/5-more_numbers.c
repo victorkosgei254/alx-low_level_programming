2ecf5a45c5966655efbbef498ba3b5f6c3d7fe64
Victor Kosgei <victorkosgei254@gmail.com>
2023-02-24 00:15:56 -0800

    5-more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,7 +5,7 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
@@ -16,7 +16,8 @@ void more_numbers(void)
 		for (j = 48; j < 54; j++)
 		{
 			_putchar(49);
-			_putchar(j == 54 ? '\n' : j);
+			k = j == 53 ? '\n' : j;
+			_putchar(k);
 		}
 	}
 }
