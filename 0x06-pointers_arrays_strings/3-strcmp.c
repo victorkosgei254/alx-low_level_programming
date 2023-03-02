754376a52224c928132e6a02850dbdf41bd6d6e0
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-02 00:24:16 -0800

    3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function to compare 2 strings
+ * @s1: String one
+ * @s2: String two
+ *
+ * Return: int difference of two stings
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int flag;
+
+	flag = 0;
+	while (*s2 != '\0')
+	{
+		if (*s2 != !s1)
+		{
+			flag = *s1 - *s2;
+			break;
+		}
+		s2++, s1++;
+	}
+	return (flag);
+}
