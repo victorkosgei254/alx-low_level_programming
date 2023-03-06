e8bbe607e6a82785a62388461a9d55ec40279632
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-06 12:43:45 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _strspn - Function that gets the length of prefix substring
+ * @s: Initial segment
+ * @accept: bytes to check
+ *
+ * Return: Int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *s_copy, *accept_copy;
+	int maxIndex, tmp;
+
+	s_copy = s;
+	accept_copy = accept;
+	maxIndex = 0, tmp = 1;
+	while (*accept != '\0')
+	{
+		while (*s != *accept)
+		{
+			s++;
+			tmp++;
+		}
+		if (*s == *accept)
+		{
+			if (maxIndex < tmp)
+			{
+				maxIndex = tmp;
+			}
+			tmp = 1;
+			s = s_copy;
+			accept++;
+		}
+		else
+		{
+			return maxIndex;
+		}
+	}
+	return maxIndex;
+}
