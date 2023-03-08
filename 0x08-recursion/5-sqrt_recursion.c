43fac6ec1610eafc343c756705613d6c014d2314
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-08 13:29:00 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,32 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * sqrt_checker - Helper function
+ * @index: Current iteration
+ * @n: number to be checked
+ *
+ * Return: square of n
+ */
 
+int sqrt_checker(int index, int n)
+{
+	if (n < 1)
+	{
+		return (-1);
+	}
+	else if (index * index == n)
+	{
+		return (index);
+	}
+	else if (index == n / 2)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_checker(index + 1, n));
+	}
+}
 /**
  * _sqrt_recursion - Find the square-root of a number
  * @n: Number to find the square root
@@ -9,5 +36,5 @@
 
 int _sqrt_recursion(int n)
 {
-	return (n);
+	return (sqrt_checker(1, n));
 }
--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -7,26 +7,20 @@
 
 int main(void)
 {
-	int r;
+int r;
 
-    r = is_prime_number(1);
+    r = _sqrt_recursion(1);
     printf("%d\n", r);
-    r = is_prime_number(1024);
+    r = _sqrt_recursion(1024);
     printf("%d\n", r);
-    r = is_prime_number(16);
+    r = _sqrt_recursion(16);
     printf("%d\n", r);
-    r = is_prime_number(17);
+    r = _sqrt_recursion(17);
     printf("%d\n", r);
-    r = is_prime_number(25);
+    r = _sqrt_recursion(25);
     printf("%d\n", r);
-    r = is_prime_number(-1);
+    r = _sqrt_recursion(-1);
     printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-
-
 	return (0);
 }
 
