0fb43e2ea130f37d193318f115f2aa8dc46f0aa9
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-11 13:05:09 -0800

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,12 @@
 /**
  * main - Entry point
  * @argc: number of commandline arguments
+ * @argv: command line arginments
  *
  * Return: (0) always
  */
 
-int main(int argc)
+int main(int argc, __attribute__((unused)) char **argv)
 {
 	argc = argc - 1;
 	_putchar(argc + '0');
