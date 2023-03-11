842a67da7bc76b22308525a2dcfd056f4f701e30
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-11 14:45:16 -0800

    libmy.a

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,2 +1,80 @@
-#include "main.c"
-
+#include "main.h"
+int _putchar(char c)
+{
+	return (0);
+}
+int _islower(int c)
+{
+	return (0);
+}
+int _isalpha(int c)
+{
+	return (0);
+}
+int _abs(int n)
+{
+	return (0);
+}
+int _isupper(int c)
+{
+	return (0);
+}
+int _isdigit(int c)
+{
+	return (0);
+}
+int _strlen(char *s)
+{
+	return (0);
+}
+void _puts(char *s)
+{
+}
+char *_strcpy(char *dest, char *src)
+{
+	return ("");
+}
+int _atoi(char *s)
+{
+	return (1);
+}
+char *_strcat(char *dest, char *src)
+{
+	return ("");
+}
+char *_strncat(char *dest, char *src, int n)
+{
+	return ("");
+}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return ("");
+}
+int _strcmp(char *s1, char *s2)
+{
+	return (0);
+}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return ("");
+}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	return ("");
+}
+char *_strchr(char *s, char c)
+{
+	return ("");
+}
+unsigned int _strspn(char *s, char *accept)
+{
+	return (0);
+}
+char *_strpbrk(char *s, char *accept)
+{
+	return ("");
+}
+char *_strstr(char *haystack, char *needle)
+{
+	return ("");
+}
