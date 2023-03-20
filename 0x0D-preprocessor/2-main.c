91f1cb73bb6eb266e65f9af9ae5aad86d7d0140a
Victor Kosgei <victorkosgei254@gmail.com>
2023-03-20 06:51:00 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Program entry point
+ *
+ * Return: (0) always
+ */
+
+int main(void)
+{
+	printf("%s", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,8 @@
+#0x0D-preprocessor
+
+** Object like Macro
+** PI
+** FILENAME
+** FUNCTION LIKE MACRO
+** SUM
+
